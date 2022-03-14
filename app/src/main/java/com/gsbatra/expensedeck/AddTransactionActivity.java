d7e9b138869f9ede66d278d07901753eba066dc1
jrisin2 <92473801+jrisin2@users.noreply.github.com>
2022-03-13 20:40:08 -0400

    Update AddTransactionActivity.java

--- app/src/main/java/com/gsbatra/expensedeck/AddTransactionActivity.java
+++ app/src/main/java/com/gsbatra/expensedeck/AddTransactionActivity.java
@@ -1,7 +1,6 @@
 package com.gsbatra.expensedeck;
 
 import android.app.DatePickerDialog;
-import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -16,18 +15,39 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.gsbatra.expensedeck.db.Transaction;
 import com.gsbatra.expensedeck.db.TransactionDatabase;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
-public class AddTransactionActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
+public class AddTransactionActivity extends AppCompatActivity {
     private int transaction_id;
+    final Calendar myCalendar = Calendar.getInstance();
     private final String[] type = new String[] {"Income", "Expense"};
     private final String[] tag = new String[] {"Utilities", "Entertainment", "Healthcare", "Transportation", "Housing",
             "Investing", "Food", "Insurance",  "Other"};
+    TextInputEditText whentext;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_add_transaction);
+
+        whentext = (TextInputEditText) findViewById(R.id.et_when);
+        DatePickerDialog.OnDateSetListener date1 = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int yr, int mt, int dy) {
+                myCalendar.set(Calendar.YEAR, yr);
+                myCalendar.set(Calendar.MONTH,mt);
+                myCalendar.set(Calendar.DAY_OF_MONTH,dy);
+                updateLabel();
+            }
+        };
+        whentext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(AddTransactionActivity.this,date1,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
         ArrayAdapter<String> typeAdapter = new ArrayAdapter<>(this, R.layout.dropdown_type, type);
         AutoCompleteTextView typeDropdown = findViewById(R.id.et_transactionType);
@@ -42,27 +62,12 @@ public class AddTransactionActivity extends AppCompatActivity implements DatePic
             setEditTransaction();
         }
         findViewById(R.id.btn_save_transaction).setOnClickListener(this::saveTransaction);
-
-        findViewById(R.id.et_when).setOnClickListener(view -> {
-            showDatePickerDialog();
-        });
     }
 
-    private void showDatePickerDialog() {
-        DatePickerDialog datePickerDialog = new DatePickerDialog(
-                this,
-                this,
-                Calendar.getInstance().get(Calendar.YEAR),
-                Calendar.getInstance().get(Calendar.MONTH),
-                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
-        );
-        datePickerDialog.show();
-    }
-
-    @Override
-    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-        String date = month + "/" + day + "/" + year;
-        ((TextInputEditText) findViewById(R.id.et_when)).setText(date);
+    private void updateLabel() {
+        String myFormat="MM/dd/yy";
+        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.US);
+        whentext.setText(dateFormat.format(myCalendar.getTime()));
     }
 
     private void setEditTransaction() {
@@ -87,14 +92,14 @@ public class AddTransactionActivity extends AppCompatActivity implements DatePic
         String type = typeTextView.getEditableText().toString();
         AutoCompleteTextView tagTextView = findViewById(R.id.et_transactionTag);
         String tag = tagTextView.getEditableText().toString();
-        String when = ((TextInputEditText) findViewById(R.id.et_when)).getText().toString();
 
-        if(title.equals("") || amount.equals("") || type.equals("") || tag.equals("") || when.equals("")){
+        if(title.equals("") || amount.equals("") || type.equals("") || tag.equals("") || whentext.equals("")){
             Toast.makeText(getApplicationContext(), "Fill out the required fields", Toast.LENGTH_SHORT).show();
             return;
         }
 
         String note = ((TextInputEditText) findViewById(R.id.et_note)).getText().toString();
+        String when = ((TextInputEditText) findViewById(R.id.et_when)).getText().toString();
 
         Transaction transaction = new Transaction(transaction_id == -1 ? 0 : transaction_id,
                 title, Double.parseDouble(amount), type, tag, note, when);
