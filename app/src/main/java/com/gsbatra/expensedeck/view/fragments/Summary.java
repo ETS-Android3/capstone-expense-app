b8b5b703b29887362a4a9e4c2ece603020d614e3
milesaway97 <78943019+milesaway97@users.noreply.github.com>
2022-03-14 18:57:20 -0400

    Add Linechart

--- app/src/main/java/com/gsbatra/expensedeck/view/fragments/Summary.java
+++ app/src/main/java/com/gsbatra/expensedeck/view/fragments/Summary.java
@@ -7,11 +7,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ExpandableListView;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,14 +20,14 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
-import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.Description;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.gsbatra.expensedeck.R;
@@ -40,13 +40,13 @@ import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Currency;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Date;
 import java.util.Calendar;
 import java.util.Locale;
 
-public class Summary extends Fragment implements TransactionAdapter.OnAmountsDataReceivedListener{
+public class Summary extends Fragment implements TransactionAdapter.OnAmountsDataReceivedListener {
     //
     Calendar rightNow = Calendar.getInstance();
     ExpandableListView expandableListView;
@@ -54,10 +54,9 @@ public class Summary extends Fragment implements TransactionAdapter.OnAmountsDat
     HashMap<String,List<String>> listItem;
     SummaryAdapter adapter;
 
-    //Expenses - currently we only have this one
     public static HashMap<String, Double> EmapMTD = new HashMap<String, Double>();
     public static HashMap<String, Double> EmapYTD = new HashMap<String, Double>();
-    //Income
+
     public static HashMap<String, Double> ImapMTD = new HashMap<String, Double>();
     public static HashMap<String, Double> ImapYTD = new HashMap<String, Double>();
 
@@ -152,7 +151,7 @@ public class Summary extends Fragment implements TransactionAdapter.OnAmountsDat
         array = getResources().getStringArray(R.array.incomelist1);
         for (String item : array) {
             list2.add(item);
-            
+            //hard-coded for now.  show income/expense for each individual item
         }
 
         listItem.put(listGroup.get(0),list1);
@@ -213,13 +212,13 @@ public class Summary extends Fragment implements TransactionAdapter.OnAmountsDat
 
                 if (yr.equals(currentyear)) {
                     if (type.equals("Expense")) {
-                        
+                        //if one of type expense doesnt exist
                         yeartotalexpenses += amt;
                         if(!EmapYTD.containsKey(tag)) {
                             //amt = amt * -1;
                             EmapYTD.put(tag, amt);
                         } else {
-                            
+                            //if one of type expense exists
                             //amt = amt * -1;
                             double y = EmapYTD.get(tag);
                             EmapYTD.replace(tag, y, y + amt);
@@ -230,7 +229,7 @@ public class Summary extends Fragment implements TransactionAdapter.OnAmountsDat
                         if(!ImapYTD.containsKey(tag)) {
                             ImapYTD.put(tag, amt);
                         } else {
-                            
+                            //if one of type income exists
                             double x = ImapYTD.get(tag);
                             ImapYTD.replace(tag, x, x + amt);
                         }
@@ -238,12 +237,12 @@ public class Summary extends Fragment implements TransactionAdapter.OnAmountsDat
 
                     if (mo.equals(currentmonth)) {
                         if (type.equals("Expense")) {
-                            
+                            //if one of type expense doesnt exist
                             monthtotalexpenses += amt;
                             if(!EmapMTD.containsKey(tag)) {
                                 EmapMTD.put(tag, amt);
                             } else {
-                               
+                                //if one of type expense exists
                                 double y = EmapMTD.get(tag);
                                 EmapMTD.replace(tag, y, y + amt);
                             }
@@ -253,7 +252,7 @@ public class Summary extends Fragment implements TransactionAdapter.OnAmountsDat
                             if(!ImapMTD.containsKey(tag)) {
                                 ImapMTD.put(tag, amt);
                             } else {
-                                
+                                //if one of type income exists
                                 double x = ImapMTD.get(tag);
                                 ImapMTD.replace(tag, x, x + amt);
                             }
@@ -304,45 +303,17 @@ public class Summary extends Fragment implements TransactionAdapter.OnAmountsDat
         balanceMTD_tv.setText(String.valueOf(format.format(balance_mtd)));
         if (balance_mtd < 0) {
             balanceMTD_tv.setTextColor(Color.RED); //if its a negative number
-            balanceMTD_tv.setText("(" + String.valueOf(format.format(balance_mtd)) + ")");
+            balanceMTD_tv.setText("(" + String.valueOf(format.format(balance_mtd * -1.0)) + ")");
         }
         TextView balanceYTD_tv = view.findViewById(R.id.list_child_YTD_balance);
         balanceYTD_tv.setText(String.valueOf(format.format(balance_ytd)));
         if (balance_ytd < 0) {
             balanceYTD_tv.setTextColor(Color.RED); //if its a negative number
-            balanceYTD_tv.setText("(" + String.valueOf(format.format(balance_ytd)) + ")");
+            balanceYTD_tv.setText("(" + String.valueOf(format.format(balance_ytd * -1.0)) + ")");
         }
 
     }
 
-
-
-    public HashMap<String, Double> getEMTDamts() {
-        return EmapMTD;
-    }
-    public HashMap<String, Double> getEYTDamts() {
-        return EmapYTD;
-    }
-
-    public HashMap<String, Double> getIMTDamts() {
-        return ImapMTD;
-    }
-    public HashMap<String, Double> getIYTDamts() {
-        return ImapYTD;
-    }
-
-    public double getMonthtotalincome() {
-        return monthtotalincome;
-    }
-    public double getMonthtotalexpenses() {
-        return monthtotalexpenses;
-    }
-    public double getYeartotalincome() {
-        return yeartotalincome;
-    }
-    public double getYeartotalexpenses() {
-        return yeartotalexpenses;
-    }
 
     public void createPieChart(HashMap<String, Integer> map){
         PieChart pieChart = view.findViewById(R.id.pieChart);
@@ -426,21 +397,17 @@ public class Summary extends Fragment implements TransactionAdapter.OnAmountsDat
 
         ArrayList<Entry> monthBalance = new ArrayList<>();
         ArrayList<Entry> yearBalance = new ArrayList<>();
-
-
     }
-
-        @Override
+    
+    @Override
     public void onAmountsDataReceived(double balance, double income, double expense, int size) {
         /*NumberFormat format = NumberFormat.getCurrencyInstance(Locale.getDefault());
         format.setCurrency(Currency.getInstance("USD"));
         String balanceMTD = format.format(balance);//
         String balanceYTD = format.format(balance);//
-
         TextView balanceMTD_tv = view.findViewById(R.id.list_child_MTD_balance);
         balanceMTD_tv.setText(balanceMTD);
         TextView balanceYTD_tv = view.findViewById(R.id.list_child_YTD_balance);
         balanceYTD_tv.setText(balanceYTD);*/
     }
 }
-
